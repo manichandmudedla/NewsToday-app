905007304988ab9969f3b0e33359dfff422878df
manichandmudedla <mudedlamanichand030102@gmail.com>
2023-09-07 11:57:47 +0530

    removing cors and express

--- app.js
+++
@@ -1,6 +0,0 @@
-const express=require('express')
-const cors=require('cors')
-const app=express()
-var port= process.env.PROT || 4000
-app.use(cors())
-app.listen(port,()=>console.log(`App started at https://localhost:${port}`))
